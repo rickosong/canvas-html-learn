13a31dc607dbaafa6e3e02b0ef3cdb020b04d166
rickosong <ricko.aprilianto00@gmail.com>
2021-08-08 10:50:04 +0800

    learn line and cicle in canvas

--- canvas.js
+++ canvas.js
@@ -5,7 +5,17 @@ canvas.height = window.innerHeight;
 
 var konteks = canvas.getContext('2d');
 
-// konteks.fillRect(x, y,width, height);
+// // konteks.fillRect(x, y,width, height);
+// konteks.fillStyle = 'blue'
+// konteks.fillRect(100, 100, 100, 100);
+// konteks.fillRect(100, 300, 100, 100);
 
-konteks.fillRect(100, 100, 100, 100);
-konteks.fillRect(100, 300, 100, 100);
+// // line
+// konteks.beginPath();
+// konteks.moveTo(800, 600);
+// konteks.lineTo(300, 600);
+// konteks.strokeStyle = 'blue';
+// konteks.stroke();
+
+// //  Arc / Circle
+// konteks.arc();
